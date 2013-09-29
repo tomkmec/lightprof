1e46ce135d8f72dce75f30d87779078fdda29fae
Tom Kmec <tom.kmec@gmail.com>
2013-09-30 00:56:49 +0200

    major refactoring; profiling standalone functions; readme.md

--- index.js
+++ index.js
@@ -3,42 +3,71 @@ var _ = require('underscore');
 _.str = require('underscore.string');
 _.mixin(_.str.exports());
 
-var divs = { ns: 1, us: 1000, ms: 1000000, s: 1000000000 };
+var timeUnits = { ns: 1, us: 1000, ms: 1000000, s: 1000000000 };
 
 var Profiler = module.exports.Profiler = function Profiler(options) {
   options = options || {};
   this.options = _.defaults(options, {
-    evalStrings: false, // when true, profiled objects and classes can be passed as Strings to .profile() method and will be evaled
-    startPaused: false,
-    treatCallbackAsReturn: true //suppose a method ends as soon as a callback (last argument of a method call if it is a function) is called.
+    // when true, profiled objects and classes can be passed as Strings to .profile() method and will be evaled
+    evalStrings: false, 
+
+    // suppose a method ends as soon as a callback (last argument of a method call if it is a function) is called.
+    treatCallbackAsReturn: true, 
+
+    // generate a key (name) for profiled method call. 
+    // This can be used to differentiate method calls for different arguments, or group different methods under one key.
+    nameFn: function (constructorName, functionName, args) {
+      return (constructorName? constructorName + '.' : '') + functionName;
+    },
+
+    // should the profiling start immediately (true), or after .resume() is called (false)
+    startPaused: false
   });
 }
 
-Profiler.prototype.profile = function() {
+Profiler.prototype.profile = function( /* what... */ ) {
+  if (!this._initialized) this.reinit();
+
+
+  for (var i=0; i<arguments.length; i++) {
+    var arg = arguments[i];
+    if (this.options.evalStrings && _.isString(arg)) arg = eval(arg);
+
+    if (_.isFunction(arg) && _.isObject(arg.prototype) && _.size(arg.prototype)>0) { // class (constructor function)
+      this._classes.push(arg);
+      this._measureObj(arg.prototype);
+
+    } else if (_.isFunction(arg)) { // plain function
+      this._functions.push(arg);
+      return this._wrap(arg, null, arg.prototype.constructor.name);
+
+    } else if (_.isObject(arg)) { // object instance
+      this._instances.push(arg);
+      this._measureObj(arg);
+
+    }
+  }
+
+}
+
+Profiler.prototype.reinit = function() {
   this.log = {};
   this.totalTime = 0;
   this.treelog = {__parent: false};
   this.instrumented = [];
+  this._functions = [];
   this._instances = [];
   this._classes = [];
-
-  var prof = this;
-  _.chain(arguments).flatten().each(function (w) {
-    if (prof.options.evalStrings && _.isString(w)) w = eval(w);
-    if(_.isFunction(w) && _.isObject(w.prototype)) prof._classes.push(w);
-    if(_.isObject(w)) prof._instances.push(w);
-  });
 
   this.stack = [];
   this.treetop = this.treelog;
   this.running = false;
 
-  _.each(prof._instances, prof._measure.bind(prof));
-  _.chain(prof._classes).pluck('prototype').each(prof._measure.bind(prof));
-
   this.running = !this.options.startPaused;
   this.totalTime = 0;
   this.startTime = this.running? process.hrtime() : false;
+
+  this._initialized = true;
 }
 
 Profiler.prototype.stop = function() {
@@ -75,147 +104,200 @@ Profiler.prototype.resume = function() {
 	this.running = true;
 }
 
-Profiler.prototype._measure = function(obj) {
+Profiler.prototype._wrap = function(fn, className, fnName) {
+  var prof = this;
+
+  return function() {
+
+    var key = prof.options.nameFn(className, fnName, arguments);
+    if ( typeof(prof.log[key]) == 'undefined' ) prof.log[key] = {calls:0, timeTotal:0, timeOwn:0}
+
+    prof.stack.push(0);
+    if (!prof.treetop[key]) prof.treetop[key] = {__calls:0, __timeTotal:0, __timeOwn:0, __parent: prof.treetop}
+    prof.treetop = prof.treetop[key];
+
+    var start = process.hrtime();
+    var ended = false;
+    var _end = function() {
+      if (ended) return;
+      ended = true;
+      var diff = process.hrtime(start);
+
+      var innerTime = prof.stack.pop()
+      prof.treetop = prof.treetop.__parent;
+      if (prof.running) {
+        var totalTime = diff[0] * 1e9 + diff[1];
+
+        prof.treetop[key].__calls++;  
+        prof.treetop[key].__timeTotal+= totalTime;  
+        prof.treetop[key].__timeOwn+= totalTime - innerTime;  
+        
+        prof.log[key].calls++;
+        prof.log[key].timeTotal+= totalTime;
+        prof.log[key].timeOwn+= totalTime - innerTime;
+        if (prof.stack.length>0) prof.stack[prof.stack.length-1]+= totalTime;
+      }
+    }
+
+    if (prof.options.treatCallbackAsReturn && typeof(arguments[arguments.length-1])=='function') {
+      var origCallback = arguments[arguments.length-1];
+      arguments[arguments.length-1] = function() {
+        _end();
+        origCallback.apply(this, arguments);//this?
+      }
+    }
+
+    var r = fn.apply(this, arguments);
+    _end();
+
+    return r;
+  }
+}
+
+
+Profiler.prototype._measureObj = function(obj) {
   var prof = this;
 
   _.chain(obj).functions().each(function(fn) {
     var orig = obj[fn];
-    var key = obj.constructor.name+'.'+fn;
-    prof.log[key] = {calls:0, timeTotal:0, timeOwn:0}
-    obj[fn] = function() {
-      prof.stack.push(0);
-      if (!prof.treetop[key]) prof.treetop[key] = {__calls:0, __timeTotal:0, __timeOwn:0, __parent: prof.treetop}
-      prof.treetop = prof.treetop[key];
-
-      var start = process.hrtime();
-      var ended = false;
-      var _end = function() {
-        if (ended) return;
-        ended = true;
-        var diff = process.hrtime(start);
-
-        var innerTime = prof.stack.pop()
-        prof.treetop = prof.treetop.__parent;
-        if (prof.running) {
-          var totalTime = diff[0] * 1e9 + diff[1];
-
-          prof.treetop[key].__calls++;  
-          prof.treetop[key].__timeTotal+= totalTime;  
-          prof.treetop[key].__timeOwn+= totalTime - innerTime;  
-          
-          prof.log[key].calls++;
-          prof.log[key].timeTotal+= totalTime;
-          prof.log[key].timeOwn+= totalTime - innerTime;
-          if (prof.stack.length>0) prof.stack[prof.stack.length-1]+= totalTime;
-        }
-      }
-      if (prof.options.treatCallbackAsReturn && typeof(arguments[arguments.length-1])=='function') {
-        var origCallback = arguments[arguments.length-1];
-        arguments[arguments.length-1] = function() {
-          _end();
-          origCallback.apply(this, arguments);//this?
-        }
-      }
-      var r = orig.apply(this, arguments);
-      _end();
-
-      return r;
-    }
+    obj[fn] = prof._wrap(orig, obj.constructor.name, fn);
     obj[fn].orig = {obj:obj, name:fn, fn:orig};
     prof.instrumented.push(obj[fn]);
   });
 }
 
 
-
 Profiler.prototype.reportPlain = function(options) {
 
   options = _.defaults(options || {}, {
-    omitZeroCalls: true,
+
+    // one of ['timeOwn', 'timeTotal', 'calls']
     orderBy: 'timeOwn',
-    timeUnit: 'ms', // s, ms, us, ns
-    format: function(fn, record, timeUnit) {
-		  var maxFnNameLength = 40; //TODO
-      if (!fn) return _.rpad("function", maxFnNameLength+1) + _.lpad("calls", 11)  + _.lpad("own time ["+timeUnit+"]", 20) + _.lpad("total time ["+timeUnit+"]", 20);
-      else return _.rpad(fn, maxFnNameLength+1) + _.lpad(record.calls, 11)  + _.lpad(record.timeOwn, 20) + _.lpad(record.timeTotal, 20);
-    }
+
+    // one of [s, ms, us, ns]
+    timeUnit: 'ms' 
   });
 
+  var maxFnNameLength = _.chain(this.log)
+    .pairs()
+    .map(function(p) { return p[0].length })
+    .max()
+    .value()
+
+  var header = 
+    _.rpad("function", maxFnNameLength+1) + 
+    _.lpad("calls", 11) + 
+    _.lpad("own time [" + options.timeUnit + "]", 20) + 
+    _.lpad("total time [" + options.timeUnit + "]", 20)
+
+  var format = function(fn, record) {
+    return _.rpad(fn, maxFnNameLength) + 
+      _.lpad(record.calls, 11) +
+      _.lpad(record.timeOwn.toFixed(2), 20) + 
+      _.lpad(record.timeTotal.toFixed(2), 20);
+  }
 
   var rows = _.chain(this.log)
     .pairs()
-    .reject(function(pair) {return options.omitZeroCalls && pair[1].calls == 0; })
-    .sortBy(function(pair) {return -pair[1][options.orderBy]})
+    .reject(function(pair) { return pair[1].calls == 0 })
+    .sortBy(function(pair) { return -pair[1][options.orderBy] })
     .map(function(pair) {
-      return options.format(
+      return format(
         pair[0], 
-        { timeOwn: pair[1].timeOwn/divs[options.timeUnit], timeTotal: pair[1].timeTotal/divs[options.timeUnit], calls: pair[1].calls }, 
-        options.timeUnit) 
+        { 
+          timeOwn: pair[1].timeOwn/timeUnits[options.timeUnit], 
+          timeTotal: pair[1].timeTotal/timeUnits[options.timeUnit], 
+          calls: pair[1].calls 
+        }
+      )
     })
     .value();
   
-  return [options.format(false,false, options.timeUnit)].concat(rows).join('\n');
+  return [header].concat(rows).join('\n');
 }
 
 Profiler.prototype.reportTree = function(options) {
 
   options = _.defaults(options || {}, {
-    timeUnit: 'ms', // s, ms, us, ns
-    format: function(fn, record, timeUnit, prefix, firstChild, lastChild) {
-		  var maxFnNameLength = 40; //TODO
-      if (!fn) return _.rpad("function", maxFnNameLength+1) + _.lpad("calls", 11)  + _.lpad("total time ["+timeUnit+"]", 20);
-      else {
-    	   var ascii = prefix + (lastChild?'\u2514':'\u251C');
-         return _.rpad(ascii+fn, maxFnNameLength) + _.lpad(record.calls, 11) + _.lpad(record.timeTotal, 20)/* + _.lpad(record.timeOwn, 20)*/;
-       }
-    }
+    timeUnit: 'ms' // s, ms, us, ns
   });
+
+  var maxFnNameLength = 40; //TODO
+
+  var header = 
+    _.rpad("function", maxFnNameLength+1) + 
+    _.lpad("calls", 11)  + 
+    _.lpad("total time ["+options.timeUnit+"]", 20)
+
+  var format = function(fn, record, prefix, firstChild, lastChild) {
+    var ascii = prefix + (lastChild? '\u2514' : '\u251C');
+    return _.rpad(ascii+fn, maxFnNameLength) + 
+      _.lpad(record.calls, 11) + 
+      _.lpad(record.timeTotal.toFixed(2), 20);
+  }
 
   var printLevel = function(root, prefix) {
     return _.chain(root)
       .pairs()
-      .reject(function(p) {return p[0].indexOf('__')==0})
-      .sortBy(function(p) {return -p[1].__timeTotal})
+      .reject(function(p) { return p[0].indexOf('__') == 0 || p[1].__calls == 0 })
+      .sortBy(function(p) { return -p[1].__timeTotal })
       .map(function(p,i,l) {
-        return [options.format(
+        var thisLevel = format(
           p[0], 
-          { timeTotal: p[1].__timeTotal/divs[options.timeUnit], timeOwn: p[1].__timeOwn/divs[options.timeUnit], calls: p[1].__calls },
-          options.timeUnit,
+          { 
+            timeTotal: p[1].__timeTotal/timeUnits[options.timeUnit], 
+            timeOwn: p[1].__timeOwn/timeUnits[options.timeUnit], 
+            calls: p[1].__calls 
+          },
           prefix,
           i==0,
           i==l.length-1
-        )].concat(printLevel(p[1],(prefix+(i==l.length-1?'\u2514':'\u2502').replace('\u2514',' ')))); 
+        );
+        var subLevels = printLevel( p[1], prefix + (i==l.length-1? '\u2514' : '\u2502').replace('\u2514',' ') )
+
+        return [thisLevel].concat(subLevels); 
       })
       .flatten()
       .value()
   }
 
-  return [options.format(false,false, options.timeUnit)].concat(printLevel(this.treelog, '')).join('\n');
-
+  return [header].concat(printLevel(this.treelog, '')).join('\n');
 }
 
 Profiler.prototype.reportHotspots = function(options) {
   options = _.defaults(options || {}, {
     timeUnit: 'ms', // s, ms, us, ns
-    rows: 10,
-    format: function(timeUnit, record) {
-      var maxFnNameLength = 40; //TODO
-      if (!record) return _.rpad("function", maxFnNameLength+1) + _.lpad("calls", 11)  + _.lpad("own time ["+timeUnit+"]", 20);
-      else {
-        var res = '';
-        for (var i=0; i<record.stack.length; i++) {
-          res += (i>0? (_.repeat(' ',i-1)+'\u2514') : '') + record.stack[i] + "\n";
-        }
-
-        return res + _.rpad((record.stack.length>0? (_.repeat(' ',record.stack.length-1)+'\u2514') : '') + record.fn, 40) + _.lpad(record.calls, 11) + _.lpad(record.time, 20)
-      }
-    }
+    records: 10
   });
 
+  var maxFnNameLength = 40; //TODO
+
+  var header = 
+    _.rpad("function", maxFnNameLength+1) + 
+    _.lpad("calls", 11) + 
+    _.lpad("own time ["+options.timeUnit+"]", 20);
+
+  var format = function(timeUnit, record) {
+    var res = '';
+    for (var i=0; i<record.stack.length; i++) {
+      res += (i>0? (_.repeat(' ',i-1)+'\u2514') : '') + record.stack[i] + "\n";
+    }
+
+    return res + 
+      _.rpad((record.stack.length>0? (_.repeat(' ',record.stack.length-1)+'\u2514') : '') + record.fn, 40) + 
+      _.lpad(record.calls, 11) + 
+      _.lpad(record.time.toFixed(2), 20);
+  }
+
   var hotspots = [];
+
   var sweep = function(root, stack) {
     for (var key in root) if (key.indexOf('__') !== 0) {
-      hotspots.push({ fn: key, time: root[key].__timeOwn/divs[options.timeUnit], calls: root[key].__calls, stack: stack });
+      hotspots.push({ 
+        fn: key, 
+        time: root[key].__timeOwn/timeUnits[options.timeUnit], 
+        calls: root[key].__calls, stack: stack 
+      });
       sweep(root[key], stack.concat([key]));
     }
   }
@@ -224,7 +306,13 @@ Profiler.prototype.reportHotspots = function(options) {
 
   hotspots = _.sortBy(hotspots, function(r) {return -r.time});
 
-  return [options.format(options.timeUnit)]
-    .concat(_.map(hotspots.slice(0,options.rows), function(r) {return options.format(options.timeUnit, r)})).join('\n');
+  var rows = _.map(
+    hotspots.slice(0, options.records), 
+    function(r) {
+      return format(options.timeUnit, r)
+    }
+  );
+
+  return [header].concat(rows).join('\n');
 
 }
--- readme.md
+++ readme.md
@@ -3,14 +3,26 @@ lightprof
 
 A lightweight profiling tool for javascript.
 
-Impersonates functions in target objects or prototypes, traces execution time and reports results.
+Wraps standalone functions or member functions in objects and classes, traces execution time and reports results.
 
-Doesn't utilize V8 profiler or anything terribly node-specific at its core so browser version should be possible and could appear someday.
+Doesn't utilize V8 profiler or anything terribly node-specific at its core so browser version should appear someday soon.
+
+Features
+--------
+
+* Profile *standalone functions*, *all functions in object*, or *all functions in a class prototype*.
+* *Customize profiler granurality* - differentiate method calls by arguments or group several methods together.
+* Don't worry about *synchronous callbacks* - they are treated as `return` by default.
+* Choose from one of three report types to get perspective you need:
+	* *Plain report*, sortable by number of calls, total execution time, or own execution time (excl. nested executions)
+	* *Tree report* to get perspective of the call stack
+	* *Hotspots report* to identify and track most time consuming calls
 
 Status
 ------
 
-Early development version. Usable, but anything can change in following versions.
+This is kind of release candidate. I'm trying to finalize basic features and API for initial stable version (1.0.0). 
+I do not plan any dramatic API changes at this point, unless I realize some serious design flaw.
 
 Example use and output
 ----------------------
@@ -30,12 +42,12 @@ var instance = new SomethingElse();
 // start profiling 'instance' object and any instances of 'Class' created after this point
 profiler.profile(instance, Class);
 
-//... do your stuff ...
+//... do your stuff, e.g. 'instance.foobar()' and 'new Class().method()' ...
 
 // stop profiling, replace traced functions with originals
 profiler.stop();
 
-//
+// output a tree report
 console.log(profiler.reportTree());
 ```
 
@@ -58,64 +70,176 @@ To get results like this:
 Important notes
 ---------------
 
-* `profile()` method replaces functions of objects and classes (constructor functions) passed as arguments to it. 
-	Instances of profiled classes which were created before `profile` was called will keep their original functions and will not be profiled.
-* 'class' names in reports are in fact names of constructor functions. You need to have named constructor function to see it 	in report: `var Class = function Class() { ... }` (and not anonymous: `var Class = function() { ... }`)
+* 'class' names in reports are in fact names of constructor functions. 
+  You need to have named constructor function to see it in report: 
+  `var Class = function Class() { ... }` (and not anonymous: `var Class = function() { ... }`)
+* the same applies for standalone functions: do not profile anonymous functions (`var fn = function() { ... }`)
 
 API
 ---
 
 ### Profiler constructor options
 
-<table>
-    <tr>
-		<th>Option</th>
-		<th>Type</th>
-		<th>Default</th>
-		<th>Description</th>
-	</tr>
-	<tr>
-		<td><code>evalStrings</code></td>
-		<td>boolean</td>
-		<td><code>false</code></td>
-		<td>When true, objects and classes in `profile()` arguments can be strings and will be evaled in that case</td>
-	</tr>
-	<tr>
-		<td><code>startPaused</code></td>
-		<td>boolean</td>
-		<td><code>false</code></td>
-		<td>When true, `profile()` will start paused (not recording function calls, just tracing call stack)</td>
-	</tr>
-	<tr>
-		<td><code>treatCallbackAsReturn</code></td>
-		<td>boolean</td>
-		<td><code>true</code></td>
-		<td>
-			When <code>false</code>, a profiled method is considered completed after return.<br>When <code>true</code>, it is considered completed either on return or when callback function is called. Callback is assumed to be the last argument passed to the profiled method (if it is a function)
-		</td>
-	</tr>
-</table>
+#### evalStrings
+
+Boolean, default: `false`
+
+When set to true, arguments passed to `profile()` may be strings which will be evaled to - 
+names of objects or classes to profile.
+
+#### startPaused
+
+Boolean, default `false`.
+
+When true, `profile()` will start paused (not recording function calls, just tracing call stack). 
+Call `resume()` to start recording in such case.
+
+#### treatCallbackAsReturn
+
+Boolean, default `true`
+
+Tells the profiler whether calling a callback function should be considered as end of function execution. 
+Callback is assumed to be the last argument passed to the profiled method (if it is a function).
+
+#### nameFn
+
+Function(constructorName, functionName, args)
+
+Default:
+```javascript
+function (constructorName, functionName, args) {
+  return (constructorName? constructorName + '.' : '') + functionName;
+}
+```
+
+Assigns a name to method call. Default implementation produces names in form of 'ClassName.methodName', 
+which results in expected profiler behavior. 
+
+However, by overriding this function, you can
+
+* add more granurality by generating different names for method calls with different arguments or 
+* reduce granurality by grouping different method calls in one name, or
+* i don't know, whatever ;)
+
+##### Example:
+
+```javascript
+var profiler = new Profiler({
+	nameFn: function (constructorName, functionName, args) {
+		var argTypes = _.chain(args).flatten()
+			.map(function(arg){ return typeof(arg) })
+			.value().join(', ');
+  		return (constructorName? constructorName + '.' : '') + functionName + '(' + argTypes + ')';
+	}
+})
+
+function doSomething() {
+	...
+}
+
+doSomething = profiler.profile(doSomething);
+
+doSomething();
+doSomething(1, 2);
+
+profiler.stop();
+profiler.reportPlain();
+// ==>
+//  function                                calls       own time [ms]     total time [ms]
+//  doSomething()                              1               12.48               12.48
+//  doSomething(number, number)                1               15.01               15.01
+```
 
 ### .profile( what... )
 
-### .stop()
+Accepts any number of arguments, each of which can be:
+* Class (= constructor function). Wraps all functions in its `prototype` object.
+* Object. Wraps all its functions.
+* Function (with empty `prototype` object). 
+  Returns wrapped function (does not continue wrapping following arguments if any). 
+  Note that you need to call wrapped function (not the original) to allow profiling - see the example above.
 
 ### .pause()
 
+Temporarily stops recording calls and execution times.
+All data and wrappers are left intact, ready to resume recording.
+
 ### .resume()
 
-### .reportPlain()
+Resume recording after paused.
 
-### .reportTree()
+### .stop()
+
+Stops profiling and replaces wrappers back with original functions.
+Collected data are kept for report methods.
+
+### .reportPlain( options )
+
+default options:
+```javascript
+{
+    orderBy: 'timeOwn', // one of ['timeOwn', 'timeTotal', 'calls']
+    timeUnit: 'ms' // one of [s, ms, us, ns]
+}
+```
+
+example output:
+
+	function                   calls       own time [ms]     total time [ms]
+	Class.method2                 8             2399.88             2399.88
+	Class.method                  5               87.05             1827.04
+	SomethingElse.bar             2               69.89               69.89
+
+### .reportTree( options )
+
+default options:
+```javascript
+{
+	timeUnit: 'ms' // one of [s, ms, us, ns]
+}
+```
+
+example output:
+
+	function                                       calls     total time [ms]
+	├Class.method3                                    2         2015.961540
+	│├Class.method                                    4         1512.021972
+	││└Class.method2                                  4         1439.995954
+	│└Class.method2                                   2          479.940796
+	└SomethingElse.bar                                1           20.926568
+
+### .reportHotspots( options )
+
+default options:
+```javascript
+{
+	timeUnit: 'ms', // one of [s, ms, us, ns]
+	records: 10 // number of hotspots reported.
+}
+```
+
+example output:
+
+	function                                       calls       own time [ms]
+	Class.method3
+	└Class.method
+	 └Class.method2                                   4             1440.01
+	Class.method3
+	└Class.method2                                    2              479.93
+	Class.method
+	└Class.method2                                    1              299.99
+	Class.method2                                     1              179.95
+
+
+### .reinit()
+
+Reinitializes profiler, clearing all data. Call this only to reuse existing Profiler instance - 
+in between `.stop(); .reportXXX()` and another `.profile()`
 
 
 What's next
 -----------
 
-* Total profile time in reports
-* Tree report improvements 
-* Events
-* Hotspots reporting
-* Asynchronous
 * Proper tests
-* Browser version?
+* Better documentation
+* Browser version
